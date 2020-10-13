29e221583527d69ccedb66ca0d2686fe8164603f
crazydeveloper09 <maciekmessi9@gmail.com>
2020-10-13 13:18:03 +0200

    First linux command line book

---
+++ Fast and Easy Linux Commands/Chapter4/name.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Enter your name"
+read name;
+echo "Welcome Mr./Mrs $name";
+echo "Thanks Mr./Mrs. $name. You have provided us with your name";
+echo "----------------------------------"
+echo "Mr./Mrs. $name, goodbye friend"
---
+++ Fast and Easy Linux Commands/Chapter4/processes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hey $USER"
+echo "Hello, I am" $USER "The following are the processes running currently"
+
+echo "List of running processes:"
+ps
