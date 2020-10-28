7606838e290886eb0acbdca4aff71725861224e7
crazydeveloper09 <maciekmessi9@gmail.com>
2020-10-28 14:30:46 +0100

    Rest of chaoter 4

--- Django 3 By Example/env/bookmarks/bookmarks/settings.py
+++ Django 3 By Example/env/bookmarks/bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a+c#x36sj6+^grd2+irv$lx5m=!n%&a)f3$dt^ouctsq&3i-1a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,20 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.google.GoogleOAuth2'
+]
+
+SOCIAL_AUTH_FACEBOOK_KEY = '***'
+SOCIAL_AUTH_FACEBOOK_SECRET = '****'
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '**'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '****'
